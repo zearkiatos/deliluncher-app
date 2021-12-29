2b2928c7daefa707ed2131ca38ded2109bb8a45d
Pedro Capriles <caprilespe@outlook.com>
2021-12-28 21:19:21 -0300

    Creating the list in the meals screen and configuration github action

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const RootStack = createStackNavigator(
   },
   {
     mode: "modal",
-    headerModal: "none"
+    headerMode: "none"
   }
 );
 
--- src/screens/Meals/index.js
+++ src/screens/Meals/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import ListItem from '../../components/ListItem'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start'
+    },
+    list: {
+        alignSelf: 'stretch'
+    }
+});
+
+const data = [{
+    _id: Date.now(), name: 'Churrasco', desc: 'Típical dish, avocado, mayonaise 🥩🍔'
+}];
 
 const Meals = () => {
     return (
         <View>
-            <Text>
-                I'm Meals
-            </Text>
+            <FlatList
+                style={styles.list}
+                data={data}
+                keyExtractor={x => x._id}
+                renderItem={({ item }) => 
+                <ListItem 
+                    onPress={() => navigation.navigate('Modal', {_id: item._id})}
+                    name={item.name}
+                />}
+            >
+
+            </FlatList>
         </View>
     );
 };
+
+Meals.navigationOptions = ({
+    title: 'Food  availables'
+});
 
 export default Meals;
