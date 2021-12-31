1f4230cb039e2edeaa9c00de8e72f8ab81951c5f
zearkiatos <caprilespe@outlook.com>
2021-12-31 00:18:22 -0300

    [Work in Progress] Using Fetch and custom hooks to get our meals

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+import {
+  NODE_ENV,
+  SERVERLESS_DELILUNCHER_API_BASE_URL
+} from "react-native-dotenv";
+const config = {
+  NODE_ENV,
+  SERVERLESS_DELILUNCHER_API_BASE_URL
+};
+
+export default config;
--- src/screens/Meals/index.js
+++ src/screens/Meals/index.js
@@ -1,44 +1,53 @@
-import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
-import ListItem from '../../components/ListItem'
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import ListItem from "../components/ListItem";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start'
-    },
-    list: {
-        alignSelf: 'stretch'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "flex-start",
+    justifyContent: "flex-start"
+  },
+  list: {
+    alignSelf: "stretch"
+  }
 });
 
-const data = [{
-    _id: Date.now(), name: 'Churrasco', desc: 'Típical dish, avocado, mayonaise 🥩🍔'
-}];
+const data = [
+  {
+    _id: Date.now(),
+    name: "Churrasco",
+    desc: "Típical dish, avocado, mayonaise 🥩🍔"
+  }
+];
 
-const Meals = () => {
-    return (
-        <View>
-            <FlatList
-                style={styles.list}
-                data={data}
-                keyExtractor={x => x._id}
-                renderItem={({ item }) => 
-                <ListItem 
-                    onPress={() => navigation.navigate('Modal', {_id: item._id})}
-                    name={item.name}
-                />}
-            >
+const Meals = ({ navigation }) => {
+  const [loading, setLoading] = useState(true);
+  const [meals, setMeals] = useState([]);
 
-            </FlatList>
-        </View>
-    );
+  const fetchMeals = async () => {
+    const response = await fetch();
+  };
+  return (
+    <View>
+      <FlatList
+        style={styles.list}
+        data={data}
+        keyExtractor={(x) => x._id}
+        renderItem={({ item }) => (
+          <ListItem
+            onPress={() => navigation.navigate("Modal", { _id: item._id })}
+            name={item.name}
+          />
+        )}
+      ></FlatList>
+    </View>
+  );
 };
 
-Meals.navigationOptions = ({
-    title: 'Food  availables'
-});
+Meals.navigationOptions = {
+  title: "Food  availables"
+};
 
 export default Meals;
