c39a2b2b7a32bfc6f4bff4a3d68f6a7c875b48d5
zearkiatos <caprilespe@outlook.com>
2022-01-02 11:48:21 -0300

    Screen detail in the modal, show the data in the user

--- src/screens/Modal/index.js
+++ src/screens/Modal/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Fragment } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import useFetch from "../../hooks/useFetch";
+import config from "../../config";
 
-const Modal = () => {
-    return (
-        <View>
-            <Text>
-                I'm a Modal
-            </Text>
-        </View>
-    );
+const Modal = ({ navigation }) => {
+  const id = navigation.getParam("_id");
+  const { loading, data } = useFetch(
+    `${config.SERVERLESS_DELILUNCHER_API_BASE_URL}/api/meals/${id}`
+  );
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <Fragment>
+          <Text>{data._id}</Text>
+          <Text>{data.name}</Text>
+          <Text>{data.description}</Text>
+        </Fragment>
+      )}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
 
 export default Modal;
