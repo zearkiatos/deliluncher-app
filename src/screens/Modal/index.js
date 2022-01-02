9d95b1637ec412d61126daaf53cdb6cc96028c6d
zearkiatos <caprilespe@outlook.com>
2022-01-02 13:02:45 -0300

    Creating the orders to our users

--- src/screens/Modal/index.js
+++ src/screens/Modal/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import useFetch from "../../hooks/useFetch";
 import config from "../../config";
 
@@ -8,6 +8,22 @@ const Modal = ({ navigation }) => {
   const { loading, data } = useFetch(
     `${config.SERVERLESS_DELILUNCHER_API_BASE_URL}/api/meals/${id}`
   );
+  const onCancel = () => navigation.navigate("Meals");
+  const onAccept = () => {
+    fetch(`${config.SERVERLESS_DELILUNCHER_API_BASE_URL}/api/orders`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        mealId: id,
+        userId: "userId"
+      })
+    }).then(() => {
+      alert("The order was generated");
+      navigation.navigate("Meals");
+    });
+  };
   return (
     <View style={styles.container}>
       {loading ? (
@@ -17,6 +33,8 @@ const Modal = ({ navigation }) => {
           <Text>{data._id}</Text>
           <Text>{data.name}</Text>
           <Text>{data.description}</Text>
+          <Button title="Accept" onPress={onAccept} />
+          <Button title="Cancel" onPress={onCancel} />
         </Fragment>
       )}
     </View>
